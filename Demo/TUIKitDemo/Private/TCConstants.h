129a17f81a411281b49d56eb055cece57d23b653
SummerHaze <wobushisummer@gmail.com>
2024-08-27 15:42:43 +0800

    Update TCConstants.h

--- Demo/TUIKitDemo/Private/TCConstants.h
+++ Demo/TUIKitDemo/Private/TCConstants.h
@@ -9,36 +9,36 @@
 #ifndef TCConstants_h
 #define TCConstants_h
 
-// 全局调度服务器地址，国内
+
 #define kGlobalDispatchServiceHost @""
 #define kApaasAppID @""
-// 全局调度服务器地址，国际
+
 #define kGlobalDispatchServiceHost_international @""
 #define kApaasAppID_international @""
-// 全局调度服务
+
 #define kGlobalDispatchServicePath @""
 
-// 开发测试环境
+
 #define kEnvDev @"/dev"
-// 生产环境
+
 #define kEnvProd @"/prod"
 
-// 获取短信验证码（含图片验证）
+
 #define kGetSmsVerfifyCodePath @""
-// 手机号登录
+
 #define kLoginByPhonePath @""
-// token 登录
+
 #define kLoginByTokenPath @""
-// 登出
+
 #define kLogoutPath @""
-// 注销账号
+
 #define kDeleteUserPath @""
 
 
-// Http配置
+
 #define kHttpServerAddr @""
 
-//Elk 数据上报地址
+
 #define DEFAULT_ELK_HOST @""
 
 //Licence url
@@ -60,18 +60,18 @@
 
 #define kHttpTimeout                         30
 
-//错误码
+
 #define kError_InvalidParam                            -10001
 #define kError_ConvertJsonFailed                       -10002
 #define kError_HttpError                               -10003
 
-//IMSDK群组相关错误码
+
 #define kError_GroupNotExist                            10010  //该群已解散
 #define kError_HasBeenGroupMember                       10013  //已经是群成员
 
-//错误信息
+
 #define  kErrorMsgNetDisconnected  @"网络异常，请检查网络"
 
-//版本
+
 #define kVersion  4
 #endif /* TCConstants_h */
